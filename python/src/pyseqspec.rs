90b188a0b3afb0743f3860cdbce190b7ca3d6cfe
Kai Zhang <zhangkai33@westlake.edu.cn>
2024-09-30 21:13:25 +0800

    update

--- python/src/pyseqspec.rs
+++ python/src/pyseqspec.rs
@@ -1,4 +1,4 @@
-use std::{path::PathBuf, str::FromStr};
+use std::{collections::HashMap, path::PathBuf, str::FromStr};
 
 use pyo3::prelude::*;
 use seqspec::{Assay, File, Modality, Read, Region, Strand, UrlType};
@@ -53,9 +53,13 @@ impl SeqSpec {
     ///   Whether the read is reverse.
     /// fastq: Path | list[Path]
     ///    The path to the fastq file containing the reads.
+    /// min_len: int
+    ///   The minimum length of the read. If not provided, the minimum length is inferred from the fastq file.
+    /// max_len: int
+    ///   The maximum length of the read. If not provided, the maximum length is inferred from the fastq file.
     #[pyo3(
-        signature = (read_id, *, modality, primer_id, is_reverse, fastq),
-        text_signature = "($self, read_id, *, modality, primer_id, is_reverse, fastq)",
+        signature = (read_id, *, modality, primer_id, is_reverse, fastq, min_len=None, max_len=None),
+        text_signature = "($self, read_id, *, modality, primer_id, is_reverse, fastq, min_len=None, max_len=None)",
     )]
     pub fn add_read(
         &mut self,
@@ -64,6 +68,8 @@ impl SeqSpec {
         primer_id: &str,
         is_reverse: bool,
         fastq: Bound<'_, PyAny>,
+        min_len: Option<usize>,
+        max_len: Option<usize>,
     ) -> Result<()> {
         let fastq = if fastq.is_instance_of::<pyo3::types::PyList>() {
             fastq.extract::<Vec<PathBuf>>()?
@@ -82,9 +88,19 @@ impl SeqSpec {
         } else {
             read.strand = Strand::Pos;
         }
+        read.read_id = read_id.to_string();
         read.modality = Modality::from_str(modality)?;
         read.primer_id = primer_id.to_string();
         read.files = Some(fastq.into_iter().map(|path| make_file_path(path)).collect::<Result<Vec<File>>>()?);
+
+        if min_len.is_none() || max_len.is_none() {
+            let len = precellar::io::get_read_length(&read, "./")?;
+            read.min_len = min_len.unwrap_or(len) as u32;
+            read.max_len = max_len.unwrap_or(len) as u32;
+        } else {
+            read.min_len = min_len.unwrap() as u32;
+            read.max_len = max_len.unwrap() as u32;
+        }
 
         reads.push(read);
         assay.sequence_spec = Some(reads);
@@ -112,16 +128,55 @@ impl SeqSpec {
 
     fn __repr__(&self) -> String {
         let assay = &self.0;
-        let tree = Tree::new("".to_string())
-            .with_leaves(assay.library_spec.as_ref().unwrap_or(&Vec::new()).iter().map(|region| build_tree(region)));
+        let mut read_list = HashMap::new();
+        if let Some(reads) = assay.sequence_spec.as_ref() {
+            for read in reads {
+                read_list.entry(read.primer_id.clone()).or_insert(Vec::new()).push(read);
+            }
+        }
+
+        let tree = Tree::new("".to_string()).with_leaves(
+            assay.library_spec.as_ref()
+                .unwrap_or(&Vec::new()).iter()
+                .map(|region| build_tree(region, &read_list))
+            );
         format!("{}", tree)
+    }
+
+    fn __str__(&self) -> String {
+        self.__repr__()
     }
 }
 
-fn build_tree(region: &Region) -> Tree<String> {
-    Tree::new(region.region_id.clone())
+fn build_tree(region: &Region, read_list: &HashMap<String, Vec<&Read>>) -> Tree<String> {
+    let id = &region.region_id;
+    let len = if region.min_len == region.max_len {
+        region.min_len.to_string()
+    } else {
+        format!("{}-{}", region.min_len, region.max_len)
+    };
+    let label = if let Some(reads) = read_list.get(id) {
+        let s = reads.iter().map(|r| format_read(r)).collect::<Vec<String>>().join(", ");
+        format!("{}({}) [{}]", id, len, s)
+    } else {
+        format!("{}({})", id, len)
+    };
+    Tree::new(label)
         .with_leaves(region.regions.as_ref().unwrap_or(&Vec::new())
-        .iter().map(|child| build_tree(child)))
+        .iter().map(|child| build_tree(child, read_list)))
+}
+
+fn format_read(read: &Read) -> String {
+    let len = if read.min_len == read.max_len {
+        read.min_len.to_string()
+    } else {
+        format!("{}-{}", read.min_len, read.max_len)
+    };
+    if read.is_reverse() {
+        format!("↑{}({})", read.read_id, len)
+    } else {
+        format!("↓{}({})", read.read_id, len)
+    }
 }
 
 fn make_file_path(path: PathBuf) -> Result<File> {
